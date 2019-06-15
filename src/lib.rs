b368f971587438a90f37cdd169391e981f97bacb
rprice <rprice@opentext.com>
2019-06-14 21:54:54 -0400

    Getting compile errors

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use exit::Exit;
 
 use std::option;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum ForthErr {
@@ -10,6 +11,8 @@ pub enum ForthErr {
     XParseErrorUserNum,
     XParseErrorGroupNum,
 }
+
+
 
 impl From<ForthErr> for i32 {
     fn from(err: ForthErr) -> Self {
@@ -32,21 +35,51 @@ impl From<option::NoneError> for ForthErr {
 #[derive(Debug)]
 pub enum Token {
     Number(u64),
-    Token(String),
+    Command(String),
 }
+
+static mut command_map: HashMap<&String, Vec<Token>> = HashMap::new();
 
 pub fn tokenize_string(s: &str) -> Vec<Token> {
     s.split_whitespace()
         .map(|x| match x.parse::<u64>() {
             Ok(n) => Token::Number(n),
-            Err(_) => Token::Token(x.to_owned()),
+            Err(_) => Token::Command(x.to_owned()),
         })
         .collect()
 }
 
 pub fn execute_token(t:Token) ->Result<(),ForthErr> {
+match t {
+    Token::Number(n)=>push_stack(n),
+    Token::Command(s)=>{
+    println!("Execute token {}", s);
+    match s.as_ref() {
+        "predefined1"=>println!("found predefined1"),
+        "predefined2"=>println!("found predefined2"),
+        s =>{
+            match command_map.get(s.as_ref()) {
+                Some(tl)=>execute_token_list(tl),
+                None=>return Err(ForthErr::UnknownToken),
+            }
+            },
+    }
+    },
+}
 
 Ok(())
+}
+
+pub fn execute_token_list(tl:&Vec<Token>) {
+        println!("Executing token list {:?}", tl);
+}
+
+fn push_stack(n:u64) {
+    println!("Pushed {} on stack", n);
+}
+
+fn pop_stack() {
+    println!("Popped stack");
 }
 
 pub fn run() ->Result<(),ForthErr> {

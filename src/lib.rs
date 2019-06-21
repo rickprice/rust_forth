e7f772974ba8284e772e4af7c95d6158197f936f
rprice <rprice@opentext.com>
2019-06-21 12:59:50 -0400

    Working on making the stack accessible

--- src/lib.rs
+++ src/lib.rs
@@ -111,6 +111,112 @@ impl ForthInterpreter {
 }
 
 impl ForthInterpreter {
+    /// This method pushes a number onto the Forth stack
+    ///
+    /// # Arguments
+    ///
+    /// * 'n' - A number to be pushed onto the top of the Forth stack
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::error::Error;
+    /// use rust_forth::ForthInterpreter;
+    /// use rust_forth::ForthError;
+    /// # use exit::Exit;
+    /// #
+    /// #   fn main() -> Result<(), ForthError> {
+    /// #
+    ///
+    ///    let mut rf = ForthInterpreter::new();
+    ///
+    ///     rf.push_stack(123);
+    ///     rf.push_stack(321);
+    ///
+    ///     rf.execute_string("ADD 2 MUL")?;
+    ///
+    ///     let n = rf.pop_stack()?;
+    ///     println!("Found {} on top of stack",n);
+    /// #
+    /// #   Ok(())
+    /// # }
+    /// ```    
+    pub fn push_stack(&mut self, n: i64) {
+        println!("Pushed {} on stack", n);
+        self.number_stack.push(n);
+    }
+
+    /// This method pops a number off the Forth stack
+    ///
+    /// # Arguments
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::error::Error;
+    /// use rust_forth::ForthInterpreter;
+    /// use rust_forth::ForthError;
+    /// # use exit::Exit;
+    /// #
+    /// #   fn main() -> Result<(), ForthError> {
+    /// #
+    ///
+    ///    let mut rf = ForthInterpreter::new();
+    ///
+    ///     rf.push_stack(123);
+    ///     rf.push_stack(321);
+    ///
+    ///     rf.execute_string("ADD 2 MUL")?;
+    ///
+    ///     let n = rf.pop_stack()?;
+    ///     println!("Found {} on top of stack",n);
+    /// #
+    /// #   Ok(())
+    /// # }
+    /// ```    
+    pub fn pop_stack(&mut self) -> Result<i64, ForthError> {
+        println!("Popped stack");
+        match self.number_stack.pop() {
+            Some(x) => Ok(x),
+            None => Err(ForthError::PopOfEmptyStack),
+        }
+    }
+
+    /// This method lets you bulk modify/read the Forth stack
+    ///
+    /// # Arguments
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::error::Error;
+    /// use rust_forth::ForthInterpreter;
+    /// use rust_forth::ForthError;
+    /// # use exit::Exit;
+    /// #
+    /// #   fn main() -> Result<(), ForthError> {
+    /// #
+    ///
+    ///    let mut rf = ForthInterpreter::new();
+    ///
+    ///     rf.push_stack(123);
+    ///     rf.push_stack(321);
+    ///
+    ///    rf.execute_string("ADD 2 MUL")?;
+    ///
+    /// let n = rf.pop_stack();
+    ///
+    ///
+    /// #
+    /// #   Ok(())
+    /// # }
+    /// ```    
+    pub fn access_stack(&mut self) -> &mut Vec<i64> {
+        &mut self.number_stack
+    }
+}
+
+impl ForthInterpreter {
     fn execute_token(&mut self, t: &Token) -> Result<(), ForthError> {
         match &self.mode {
             Mode::Interpreting => {
@@ -282,21 +388,6 @@ impl ForthInterpreter {
         println!("Duplicated {} ", x);
 
         Ok(())
-    }
-}
-
-impl ForthInterpreter {
-    fn push_stack(&mut self, n: i64) {
-        println!("Pushed {} on stack", n);
-        self.number_stack.push(n);
-    }
-
-    fn pop_stack(&mut self) -> Result<i64, ForthError> {
-        println!("Popped stack");
-        match self.number_stack.pop() {
-            Some(x) => Ok(x),
-            None => Err(ForthError::PopOfEmptyStack),
-        }
     }
 }
 

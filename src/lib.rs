e2169f56349946e73b2efab326ff785bf36b4342
rprice <rprice@opentext.com>
2019-08-02 10:56:30 -0400

    Cleaning up code. Need to recover doc tests though.

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,10 @@
 
 pub use error::ForthError;
 pub use forth_compiler::Token;
-pub use state::State;
 
 pub mod error;
 pub mod forth_compiler;
 pub mod stack_machine;
-mod state;
 
 pub enum Handled {
     Handled,
